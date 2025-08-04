d29f231e8d353a673c87777deda47d640e7b7442
saicharan2025-ui <saicharandaripalli@gmail.com>
2025-08-04 16:44:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.get("/api/today-sessions", async (req, res) => {
   }
 });
 
-const Session = require("./models/Session"); // Ensure this line is at the top if not already
+const Session = require("./sessions.js"); // Ensure this line is at the top if not already
 
 // Get sessions for today or past days
 app.get("/api/get-sessions", async (req, res) => {
