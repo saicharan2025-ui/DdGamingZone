9926cac7fab018705ca1cb5d481146ff646a6e18
saicharan2025-ui <saicharandaripalli@gmail.com>
2025-08-04 13:45:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
